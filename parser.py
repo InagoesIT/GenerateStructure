25240ef2bf1a8f845831781d10b50f17ea246fd8
ina <wenulife@gmail.com>
2022-12-01 12:44:59 +0200

    project structure created

---
+++ parser.py
@@ -0,0 +1,5 @@
+class Parser:
+    def __init__(self, root_path, json_path):
+        self.root_path = root_path
+        self.json_path = json_path
+
---
+++ structure_generator.py
@@ -0,0 +1,7 @@
+class StructureGenerator:
+    def __init__(self, root_path, json_path):
+        self.root_path = root_path
+        self.json_path = json_path
+
+    def run(self):
+        pass
